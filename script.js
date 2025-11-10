7bd984ad2925e79447260b7804a823b51dbd2413
Hamser1001 <22420672+Hamser1001@users.noreply.github.com>
2025-11-10 15:08:51 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ script.js
@@ -0,0 +1,34 @@
+const textInput = document.getElementById("text-input");
+const checkBtn = document.getElementById("check-btn");
+const result = document.getElementById("result");
+
+
+const cleanText = (text) => {
+    const regex = /[^a-zA-Z0-9]/g;
+    const cleanedText = text.replace(regex, '');
+    return cleanedText
+}
+
+
+const reverseFunction = (text) => {
+    const reversedText = text.toLowerCase().split("").reverse().join("");
+    return reversedText;
+}
+
+
+checkBtn.addEventListener("click", () => {
+    const cleanedtext = cleanText(textInput.value).toLowerCase().split("").join("")
+    const text = reverseFunction(cleanedtext);
+    if (cleanedtext == "") {
+        alert("Please input a value")
+    } else if (cleanedtext == text) {
+        result.innerText = `${textInput.value} is a Palindrome`;
+    } else {
+        result.innerText = `${textInput.value} is not a Palindrome`;
+    }
+    // clear the text input after one minute
+    setTimeout(() => {
+        textInput.value = "";  // clears the input
+        result.innerText = "";  // clears the result
+    }, 60000);
+});
